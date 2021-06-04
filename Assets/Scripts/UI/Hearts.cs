fa82674d2b7953978e03c21a7827bdf6210870a3
Steven Hall <stevenhall.art@gmail.com>
2021-06-05 00:02:32 +0100

    Pull para rafa

--- Assets/Scripts/UI/Hearts.cs
+++ Assets/Scripts/UI/Hearts.cs
@@ -8,20 +8,24 @@ public class Hearts : MonoBehaviour
     public Image[] Lives;
     public int RemainingLives;
 
-
-
-    public void LoseLife()
+    public void LifeLost()
     {
         // remove lives with decrementation
         RemainingLives--;
         // Removal of heart Icon
-
+        Lives[RemainingLives].enabled = false;
         // if we have no lifes left
-    }
+        if (RemainingLives == 0)
+        {
+            Debug.Log("No lives left");
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Return))
+            {
+                LifeLost();
+            }
+        }
     }
 }
