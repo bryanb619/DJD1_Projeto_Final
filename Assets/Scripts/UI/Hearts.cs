32473687d8700914287d794eacc807b79317ef69
Steven Hall <stevenhall.art@gmail.com>
2021-06-05 15:34:17 +0100

    Correção de Merge

---
+++ Assets/Scripts/UI/Hearts.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hearts : MonoBehaviour
+{
+    public Image[] Lives;
+    public int RemainingLives;
+
+
+
+    public void LoseLife()
+    {
+        // remove lives with decrementation
+        RemainingLives--;
+        // Removal of heart Icon
+
+        // if we have no lifes left
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
