bdcd465953f7045b3a0b87759555cb07d3cae712
Steven Hall <stevenhall.art@gmail.com>
2021-06-05 20:36:43 +0100

    EnemyCollision Basic

--- Assets/Scripts/Enemy/EnemyCollision.cs
+++ Assets/Scripts/Enemy/EnemyCollision.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
+
 public class EnemyCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    void StartTriggering()
     {
-        
+        InitTrigger();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitTrigger()
     {
-        
+        //  Turns trigger on
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+    // detection of collision (takes health, not now...)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log($"{name} Contact");
+        }
     }
 }
+
+
