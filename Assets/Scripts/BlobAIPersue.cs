f343fd4b9ef7d69cbf0ac253a2479f1c1abf7bd1
Steven Hall <stevenhall.art@gmail.com>
2021-06-05 01:09:27 +0100

    Merged Still attempt 2

---
+++ Assets/Scripts/BlobAIPersue.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobAIPersue : MonoBehaviour
+{
+    public GameObject PlayeObject;
+    [SerializeField]
+    private Transform PlayerPosition;
+    private Vector2 currentPos;
+    public float distance;
+    [SerializeField]
+    private float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        PlayerPosition = PlayerPosition.GetComponent<Transform>();
+        currentPos = GetComponent<Transform>().position;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector2.Distance(transform.position, PlayerPosition.position) < distance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, PlayerPosition.position, Speed * Time.deltaTime);
+        }
+        else
+        {   
+            if(Vector2.Distance(transform.position, currentPos)<= 0)
+            {
+
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, currentPos, Speed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public Transform FirePos;
+    public GameObject Fire;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player input (rato, lado esquerdo(0 por default))
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // calling do método
+            shoot();
+
+        }
+        // Código de disparo
+        void shoot()
+        {
+            Instantiate(Fire, FirePos.position, FirePos.rotation);
+
+        }
+
+
+    }
+}
