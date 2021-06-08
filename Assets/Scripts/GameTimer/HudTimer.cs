9d33390a82b6649e4e8f13208bb58a35f53e315c
Sonia Raposo <soniaraposo.art@gmail.com>
2021-06-08 19:48:35 +0100

    Timer Score Hud Basic Update 2

---
+++ Assets/Scripts/GameTimer/HudTimer.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudTimer : MonoBehaviour
+{
+    // Starting time = zero
+    public float startTime;
+    // UI Text in canvas
+    public Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // float to string conversion
+        timerText.text = startTime.ToString("F2"); 
+
+    }
+    // Update is called once per frame
+    // Time update per FPS
+    void Update()
+    {   
+        // timer using delta dime to count every second
+        startTime += Time.deltaTime;
+        // conversion to string
+        timerText.text = startTime.ToString("F2");
+    }
+    
+       
+
+    
+}
